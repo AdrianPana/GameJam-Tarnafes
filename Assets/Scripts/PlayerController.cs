b73840c2ebfb57a7d549387accf28d51415886fc
Adi Pana <43998334+AdrianPana@users.noreply.github.com>
2024-04-05 17:57:23 +0300

    PlayerMovement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private bool isMoving;
+    private Vector3 origPos, targetPos;
+    private bool newInput = false;
+
+    PlayerInputActions inputControls;
+    Vector2 currentDirection = Vector2.zero;
+
+    [SerializeField]
+    private float timeToMove = 0.2f;
+
+    void Start()
+    {
+        inputControls = new PlayerInputActions();
+        inputControls.Enable();
+    }
+
+    void Update()
+    {
+        Vector2 input = inputControls.BaseCharacter.Move.ReadValue<Vector2>();
+
+        //if (input.x != 0 && input.y != 0 && !newInput)
+        //{
+        //    newInput = true;
+        //    if (Mathf.RoundToInt(input.x) == Mathf.RoundToInt(currentDirection.x))
+        //    {
+        //        currentDirection = new Vector2(0, input.y);
+        //    }
+        //    else
+        //    {
+        //        currentDirection = new Vector2(input.x, 0);
+        //    }
+        //}
+        //else
+        //{
+        //    newInput = false;
+        currentDirection = input;
+        //}
+
+        if (currentDirection.y > 0 && !isMoving)
+        {
+            StartCoroutine(MovePlayer(Vector3.up)); 
+        }
+        if (currentDirection.y < 0 && !isMoving)
+        {
+            StartCoroutine(MovePlayer(Vector3.down));
+        }
+        if (currentDirection.x > 0 && !isMoving)
+        {
+            StartCoroutine(MovePlayer(Vector3.right));
+        }
+        if (currentDirection.x < 0 && !isMoving)
+        {
+            StartCoroutine(MovePlayer(Vector3.left));
+        }
+    }
+
+    private IEnumerator MovePlayer(Vector3 direction)
+    {
+        isMoving = true;
+
+        float elapsedTime = 0;
+
+        origPos = transform.position;
+        targetPos = origPos + direction;
+
+        while (elapsedTime < timeToMove)
+        {
+            transform.position = Vector3.Lerp(origPos, targetPos, (elapsedTime / timeToMove));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPos;
+
+        isMoving = false;
+    }
+}
